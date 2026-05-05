d0fc6df9bf44c3111acb784bf3651685208407cf
Prabhakaran Jayapal <prabhakaranjayapal26@gmail.com>
2026-05-05 11:07:10 +0530

    Added welcome

--- LeapYear.java
+++ LeapYear.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
+        System.out.println("Welcome to Leap year program");
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
